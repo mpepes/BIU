2e50d6621a4b9acb435dff224144bfe02ac847a2
mpepes <piotrmichalek89@gmail.com>
2021-06-30 01:58:15 +0200

    Add cart badge items counter

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CartService } from "../services/cart.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  constructor(private cartService: CartService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get totalItemsCount(): number {
+    return this.cartService.items.length
   }
-
 }
